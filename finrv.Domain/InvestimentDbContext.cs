69cf7f9ad116c794e4c7da76436ef047611e736d
davypaulino <davy.paulino.dantas@gmail.com>
2025-06-04 21:52:03 -0300

    feat: added database connection

---
+++ finrv.Domain/InvestimentDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace finrv.Domain;
+
+public class InvestimentDbContext : DbContext
+{
+
+    public InvestimentDbContext(DbContextOptions<InvestimentDbContext> options) : base(options) {
+        Console.WriteLine("Hellow");
+    }
+}
